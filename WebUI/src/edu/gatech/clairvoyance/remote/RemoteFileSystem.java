2c094573e6051d5cb448e80f129052df47bda38d
Xiangyu Li <xiangyu@invent.cc.gt.atl.ga.us>
2013-03-05 17:33:35 -0500

    030413

--- WebUI/src/edu/gatech/clairvoyance/remote/RemoteFileSystem.java
+++ WebUI/src/edu/gatech/clairvoyance/remote/RemoteFileSystem.java
@@ -1,5 +1,46 @@
 package edu.gatech.clairvoyance.remote;
-
+import com.jcraft.jsch.*;
 public class RemoteFileSystem {
+	private String hostName;
+	private String username;
+	private String password;
+	private Channel shell;
+	
+	public RemoteFileSystem(String hostName){
+		setHostName(hostName);
+	}
+	
+	public RemoteFileSystem(String hostName, String username, String password){
+		this(hostName);
+		setUserConfidentials(username,password);
+	}
+	
+	public void setHostName(String hostname){
+		if(hostname == null){
+			throw new IllegalArgumentException("Host name should not be null.");
+		}
+		this.hostName=hostname;
+	}
+	
+	public void setUserConfidentials(String username, String password){
+		if(username == null){
+			throw new IllegalArgumentException("Username should not be null.");
+		}
+		this.username=username;
+		if(password == null){
+			throw new IllegalArgumentException("Password should not be null.");
+		}
+		this.password=password;
+	}
+	
+	private Channel getShellChannel(){
+		assert(hostName != null);
+		if(username == null || password == null ){
+			throw new IllegalStateException("User confidential is not specified.");
+		}
+		JSch jsch=new JSch();
+		
+		return null;
+	}
 
 }
